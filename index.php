57aa5e48aec1df5061f936c8655fe1d5fea39924
Gorkiyspb <gorkiyspb@gmail.com>
2018-02-04 18:58:08 +0300

    12

---
+++ index.php
@@ -0,0 +1,128 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8" >
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+<link rel="stylesheet" href="style/rules.css">	
+<link rel="stylesheet" href="style/style.css">
+<link rel="stylesheet" href="style/menu.css">
+<link rel="stylesheet" href="style/content.css">
+
+
+<title>наша семья</title>
+</head>
+
+<body>
+
+	<header>
+	<nav>
+		<ul>
+			<li><a href="#">о нас</a></li>
+			<li><a href="#">полезности</a></li>
+			<li><a href="#">путешествия</a></li>
+			<li><a href="#">галерея</a></li>
+		</ul>
+	</nav>
+		<img alt="" src="images/hover.png">
+<div id="line">
+	<a id="exit" href="#"><div id="exit_img"></div></a>
+	<div id="user"></div>
+	<div id="weather"></div>
+	<div id="location"></div>
+	<div id="time"></div>
+	<div id="data"></div>	
+</div>		
+	</header>
+	
+	
+	
+<section>
+	<menu>
+		<ul>
+			<li><a href="#"><div id="key"></div></a></li> 
+			<li><a href="#"><div id="mail"></div></a></li>
+		</ul>
+			</menu>
+
+<div id="block">
+<div id="rotator"></div>
+	<!--<div id="centr">-->
+	
+	<table>
+		<tr>
+			<td id="first">
+				<div id="first_b">
+					<h2 id="h2">Здравствуйте, этот сайт создан в основном для наших родствеников и друзей. Мы будем рады если размещенные здесь материалы будут Вам интересны и полезны. </h2>
+				<div id="empty"></div>
+			<h1>Наши новости:</h1>
+<div id="news">
+<div class="news">
+	<div class="news_img"></div>
+		<div class="text">
+	<div>
+			<span>25.10.2017</span>
+		<span class="p">Рейтинговое решение связано с тем, что Венесуэла не выплатила проценты держателям облигаций нефтяной госкомпании PDVSA с погашением в октябре 2019 г. и октябре 2024 г. 13 ноября истекла 30-дневная отсрочка для этих выплат. Рейтинг этих бондов Fitch снизило до D (дефолт) и подтвердила рейтинги других выпусков госбумаг на уровне С. </span>
+	</div>
+	</div>
+</div>
+			
+<div class="news">
+	<div class="news_img"></div>
+		<div class="text">
+	<div>
+			<span>25.10.2017</span>
+			<span class="p">Рейтинговое решение связано с тем, что Венесуэла не выплатила проценты держателям облигаций нефтяной госкомпании PDVSA с погашением в октябре 2019 г. и октябре 2024 г. 13 ноября истекла 30-дневная отсрочка для этих выплат. Рейтинг этих бондов Fitch снизило до D (дефолт) и подтвердила рейтинги других выпусков госбумаг на уровне С. </span>
+			</div>
+	</div>
+</div>
+		
+<div class="news">
+	<div class="news_img"></div>
+		<div class="text">
+	<div>
+			<span>25.10.2017</span>
+			<span class="p">Рейтинговое решение связано с тем, что Венесуэла не выплатила проценты держателям облигаций нефтяной госкомпании PDVSA с погашением в октябре 2019 г. и октябре 2024 г. 13 ноября истекла 30-дневная отсрочка для этих выплат. Рейтинг этих бондов Fitch снизило до D (дефолт) и подтвердила рейтинги других выпусков госбумаг на уровне С. </span>
+	</div>
+		</div>
+</div>					
+								
+<div class="news">
+	<div class="news_img"></div>
+		<div class="text">
+	<div>
+			<span>25.10.2017</span>
+			<span class="p">Рейтинговое решение связано с тем, что Венесуэла не выплатила проценты держателям облигаций нефтяной госкомпании PDVSA с погашением в октябре 2019 г. и октябре 2024 г. 13 ноября истекла 30-дневная отсрочка для этих выплат. Рейтинг этих бондов Fitch снизило до D (дефолт) и подтвердила рейтинги других выпусков госбумаг на уровне С. </span>
+			</div>
+	</div>
+</div>											
+																	
+</div>			
+				</div>
+			</td>
+			<td id="second">
+			<h3>Наш выбор:</h3>
+				<div id="second_d">
+					<a href="https://www.detmir.ru/" target="_blank"><div></div></a>
+				</div>
+			</td>
+		</tr>
+	</table>
+<!--</div>-->
+</div>			
+
+						
+							
+						
+	
+</section>
+	<footer>
+		<span>2017г.</span>
+	</footer>
+
+</body>
+</html>
+
+
+
+
