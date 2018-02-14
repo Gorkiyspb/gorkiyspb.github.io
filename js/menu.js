be5c6c6a61af4855f0c223f2ab6ccb70a9465689
gorkiyspb <gorkiyspb@gmail.com>
2018-02-14 10:31:47 +0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -5,15 +5,19 @@ var a='о нас';
 var b='полезности';
 var c='путешествия';	
 var d='галерея';
-
 	
 	$('nav').append('<ul></ul>');
-	$('ul').append('<li><a href="#">'+a+'</a></li>');
-	$('ul').append('<li><a href="#">'+b+'</a></li>');
-	$('ul').append('<li><a href="#">'+c+'</a></li>');
-	$('ul').append('<li><a href="#">'+d+'</a></li>');
 	
-	$('a').eq(i).focusin(function(){$(this).addClass('.focus_a');$('nav ul li').eq(i).append('<ul></ul>');});
+	$('nav ul').append('<li><a href="#">'+a+'</a></li>');
+	$('nav ul').append('<li><a href="#">'+b+'</a></li>');
+	$('nav ul').append('<li><a href="#">'+c+'</a></li>');
+	$('nav ul').append('<li><a href="#">'+d+'</a></li>');
+	$('nav ul li').append('<ul></ul>');
+	var i=$('nav ul li ul');	
+var e=0;
+	$('nav ul li a').click(function(){$(i).eq(e).animate({'margin-top':100+'px'});});
+								 
+	
 	
 	/*function focus(){if(i<4){
 		
