ca775860d899f3224ab40c0579e6b057bad37d66
gorkiyspb <gorkiyspb@gmail.com>
2018-02-18 12:57:00 +0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -7,20 +7,32 @@ var c='путешествия';
 var d='галерея';
 	
 	$('nav').append('<ul></ul>');
-	
+
 	$('nav ul').append('<li><a href="#">'+a+'</a></li>');
 	$('nav ul').append('<li><a href="#">'+b+'</a></li>');
 	$('nav ul').append('<li><a href="#">'+c+'</a></li>');
 	$('nav ul').append('<li><a href="#">'+d+'</a></li>');
-	$('nav ul li').append('<ul></ul>');
-	/*var i=$('nav ul li ul');	
-var e=0;
-	$('nav ul li a').click(function(){$(i).eq(e).animate({'margin-top':100+'px'});});*/
-								 
+		
 	
-	
-			
+var menu=('nav ul li a');
 	
 
+
+$(menu).click(function(){$(this).addClass('a');$(this).after('<ul></ul>');
+	$('nav ul li ul').animate({marginTop:-42+'px'},1000);});
+	
+
+	
+	
+	/*function focus(){if(i<4){
+		
+		$('a').eq(i).focusin(function(){$(this).addClass('.focus_a');});
+		$('nav ul li').eq(i).append('<ul></ul>');}}					
+	
+	focus();*/
+		
+	
+//
+//$('nav ul li ul').append('<li></li>');
 
 });
