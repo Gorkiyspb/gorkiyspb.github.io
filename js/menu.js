8f2e0e40174f01a6378fea53c410670f00959e59
gorkiyspb <gorkiyspb@gmail.com>
2018-02-25 18:16:20 +0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,27 @@
 // JavaScript Document
 $(document).ready(function createMenu(){
 'use strict';
+var smenu=('nav ul li a');	
 $('nav').append('<ul></ul>');
 var t=['о нас','полезности','путешествия','галерея'];
-for(var i=0;i<t.length;i++){$('nav ul').append('<li><a href="#">'+t[i]+'</a></li>');}
-
-function createSecondmenu(){var smenu=('nav ul li a');
-							
-$(smenu).on('click',function(){var ind=$(this).index(smenu);
-if($(smenu).hasClass('a')){$(smenu).removeClass('a');
-$('nav ul li ul').animate({bottom:110+'px'},350);
-$(this).addClass('a');$('nav ul li ul').eq(ind).animate({bottom:-100+'%'},350);}
-else{$(this).addClass('a');$(smenu).after('<ul></ul>');
-$('nav ul li ul').eq(ind).animate({bottom:-100+'%'},350);
-$('nav ul li ul:first').css({display:'none'});}
-});}
-createSecondmenu();	
+for(var i=0;i<t.length;i++)
+	{$('nav ul').append('<li><a href="#">'+t[i]+'</a></li>');}
 	
+$('nav ul li').append('<ul></ul>');
+var ul=$('nav ul li ul');
+	
+function createSecondmenu(){
+
+$(smenu).on('click',function(){
+	if($(smenu).hasClass('a')){$(smenu).removeAttr('class');
+		$(ul).stop().animate({bottom:80+'px'},500);
+			$(this).addClass('a');									  				   
+				$(this).next(ul).stop().animate({bottom:-100+'%'},500);}										
+else	
+	{$(this).addClass('a');
+	 	$(this).next(ul).animate({bottom:-100+'%'},500);
+ 			$('nav ul li ul:first').css({display:'none'});}});}
+
+createSecondmenu();		
 
 });
