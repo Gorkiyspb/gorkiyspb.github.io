337562e582a8292aacde246518929efaff933ee4
gorkiyspb <gorkiyspb@gmail.com>
2018-12-17 14:46:13 +0300

    Update rotator.js

--- js/rotator.js
+++ js/rotator.js
@@ -56,7 +56,6 @@ $('.img').attr('src',img[r]);animeslide(r);
 rotator_prew1();});
 var rotator_prew1=function(){stopAnimate();for(var f=0;f<10;f++){$('#rotator').append('<div class="stripe" style="width:100%" name="'+f+'" ><img class="img" alt="img"  src="'+img[t]+'"></div>');$('.img').css({opacity:0});							 
 $('.img').animate({opacity:1},2000);}};
-
 var slideTime; 
 var stop=false;	
 function start(){if(stop!=false){slideTime=setTimeout(function(){animeslide('next');},pause);}
