d6e7fae142cf3e4c6428a349a437b94e12b14338
gorkiyspb <gorkiyspb@gmail.com>
2018-12-17 14:42:15 +0300

    Update rotator.js

--- js/rotator.js
+++ js/rotator.js
@@ -54,7 +54,6 @@ $('[name='+f+']').animate({opacity:0},speed*f);}};
 $('.point').click(function(){var r=parseFloat($(this).text());						 
 $('.img').attr('src',img[r]);animeslide(r);
 rotator_prew1();});
-	
 var rotator_prew1=function(){stopAnimate();for(var f=0;f<10;f++){$('#rotator').append('<div class="stripe" style="width:100%" name="'+f+'" ><img class="img" alt="img"  src="'+img[t]+'"></div>');$('.img').css({opacity:0});							 
 $('.img').animate({opacity:1},2000);}};
 
