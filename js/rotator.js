d2eebcd2d900021329fd317d6f6bdef02e004500
gorkiyspb <gorkiyspb@gmail.com>
2018-12-28 14:21:18 +0300

    Update rotator.js

--- js/rotator.js
+++ js/rotator.js
@@ -59,7 +59,7 @@ $('.img').animate({opacity:1},2000);}};
 var slideTime; 
 var stop=false;	
 function start(){if(!stop){slideTime=setTimeout(function(){start();},pause);animeslide('next');}	 
-$('#rotator').hover(function(){clearTimeout(slideTime);},function(stop){stop=true;});};
+$('#rotator').hover(function(){clearTimeout(slideTime);},function(){stop=true;});};
 
 	start(stop);});
 
